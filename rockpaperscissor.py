19e9e02958010b6cf1c8f38e8a25aa8fc7a4374b
Kirtan075 <kirtanp046@gmail.com>
2024-11-11 16:03:56 -0500

    2nd

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,6 +1,10 @@
 def main():
     print("Welcome to Rock Paper Scissors!")
-    # We'll add more code here later...
+    print("Here are the rules:")
+    print("1. Rock beats Scissors")
+    print("2. Scissors beats Paper")
+    print("3. Paper beats Rock")
+    print("4. If both players select the same option, it's a tie.")
 
 if __name__ == "__main__":
     main()
